75cd7508b03d2a522b5a0154005a746d078e4654
azimthariq9 <azimthariq9@gmail.com>
2026-02-07 20:53:12 +0700

    fitur list product

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
@@ -14,24 +14,3 @@ products: List[Product] = []
 @app.get("/products")
 def list_products():
     return products
-
-@app.post("/products")
-def add_product(product: Product):
-    products.append(product)
-    return {"message": "Produk ditambahkan", "data": product}
-
-@app.put("/products/{product_id}")
-def update_product(product_id: int, updated: Product):
-    for i, p in enumerate(products):
-        if p.id == product_id:
-            products[i] = updated
-            return {"message": "Produk diupdate", "data": updated}
-    raise HTTPException(status_code=404, detail="Produk tidak ditemukan")
-
-@app.delete("/products/{product_id}")
-def delete_product(product_id: int):
-    for p in products:
-        if p.id == product_id:
-            products.remove(p)
-            return {"message": "Produk dihapus"}
-    raise HTTPException(status_code=404, detail="Produk tidak ditemukan")
